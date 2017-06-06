f7cef02e263df165d5c359d8585f7367bb901237
Pavlenko Svetoslav <Svet6os6laV6@yandex.ru>
2017-06-06 11:17:43 +0300

    api

--- index.php
+++ index.php
@@ -14,15 +14,16 @@
     $text = $message["text"];
     // Пример обработки команды
  
-
+    	if ($text =='/start'){
     	/*$url = "http://apideliverycity.ru//api/orders?sid=".$sid."&token=".$token;*/
     	$url = "http://apideliverycity.ru/api/orders?sid=hfgjixbl&token=cxUUe9jdFLALvhZKa6niBC9UFvc5BSQRnDNxi9Ex";
     	$updates = json_decode($url, TRUE);
         $messages = json_decode($updates, TRUE);
 
        // Отправляем сформированное сообщение обратно в Telegram пользователю   
-    	sendMessage($website, $chatId, $message);
-        function sendMessage($website, $chatId, $message){
+    	// sendMessage($website, $chatId, $message);
+        // function sendMessage($website, $chatId, $message){
        file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$messages);
-       }    
+       // }    
+   }
 ?>
