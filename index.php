4db9f6761612fa091607fe556a68db5228b3127a
Pavlenko Svetoslav <Svet6os6laV6@yandex.ru>
2017-06-06 12:17:43 +0300

    fix

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
-
     set_time_limit(0);
-
 
     $botToken = "257002841:AAF73D-lPHhpnqru8jp-R9qPq5FPM1KSOx8";
     $website = "https://api.telegram.org/bot".$botToken;
-    
     $content = file_get_contents("php://input");
     $update = json_decode($content, TRUE);
     $message = $update["message"];
     $chatId = $message["chat"]["id"];
     $text = $message["text"];
-switch ($text ) {
+ switch ($text ) {
 	case 'hi':
 		$messages = 'Привет';
 		sendMessage($website, $chatId, $messages);
 		break;
 }
      
-         function sendMessage($website, $chatId, $message){
+         function sendMessage($website, $chatId, $messages){
         file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$messages);
        }    
 ?>
