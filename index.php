b461083523e8d6ef1ef8cb4f03c53670744318d3
Pavlenko Svetoslav <Svet6os6laV6@yandex.ru>
2017-06-06 15:08:45 +0300

    fix

--- index.php
+++ index.php
@@ -10,26 +10,23 @@
     $text = $message["text"];
 
 
-	    $url = "http://apideliverycity.ru/api/orders?sid=hfgjixbl&token=cxUUe9jdFLALvhZKa6niBC9UFvc5BSQRnDNxi9Ex";
-    	$content = file_get_contents('http://apideliverycity.ru/api/orders?sid=hfgjixbl&token=cxUUe9jdFLALvhZKa6niBC9UFvc5BSQRnDNxi9Ex');
-        $mass = json_decode($content, TRUE);
-        $messages = var_dump($mass);
-         //  var_dump($mass['orders'][0]['items']['title']);
-// foreach ($mass->orders as $key => $value) {
-// 	echo $value;
-// }
+    	if($text == '/start'){
 
-         file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$messages);
+    			$messages ='Введите Sid за тем токен';
+    			sendMessage($website, $chatId, $messages);
+    			$content = file_get_contents("php://input");
+    			 $id = $message["text"];
 
-
-
-
-
-
-
-
-
+    	}
 
          function sendMessage($website, $chatId, $messages){
         file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$messages);
        }
+
+       function apiDelivery($id, $token){
+       	$url = "http://apideliverycity.ru/api/orders?sid=".$id."&token=".$token;
+    	$content = file_get_contents($url);
+        $mass = json_decode($content, TRUE);
+        $messages = var_dump($mass);
+        sendMessage($website, $chatId, $messages);
+       }    
