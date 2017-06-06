022eca2fbddc2ac9049c890ec85b3e3711ba9625
Pavlenko Svetoslav <Svet6os6laV6@yandex.ru>
2017-06-06 17:10:37 +0300

    fix

--- index.php
+++ index.php
@@ -14,16 +14,16 @@ $token = 'cxUUe9jdFLALvhZKa6niBC9UFvc5BSQRnDNxi9Ex';
 	$url = "http://apideliverycity.ru/api/orders?sid=".$sid."&token=".$token.'&limit=6';
     	$content = file_get_contents($url);
         $mass = json_decode($content, TRUE);
-            		for($i=0;$i<=6;$i++){
-            			for ($k=0; $k <=6 ; $k++) { 
+            		for($i=0;$i<=3;$i++){
+            			for ($k=0; $k<=3 ; $k++) { 
     				echo $mass['orders'][$i]['fields'][0]['title'].': '.$mass['orders'][$i]['fields'][0]['description']."<br> ".$mass['orders'][$i]['date']."<br> ".$mass['orders'][$i]['fields'][1]['title'].': '.$mass['orders'][$i]['fields'][1]['description']."<br> ".$mass['orders'][$i]['fields'][2]['title'].': '.$mass['orders'][$i]['fields'][2]['description']."<br> ".$mass['orders'][$i]['fields'][3]['title'].': '.$mass['orders'][$i]['fields'][3]['description']."<br> ".'ID '.$mass['orders'][$i]['items'][0]['id']."<br> Наименование: ".$mass['orders'][$i]['items'][0]['title'].' '.$mass['orders'][$i]['items'][0]['quantity']."шт. <br> ".$mass['orders'][$i]['items'][0]['price'].' '.$mass['orders'][$i]['items'][0]['currency']."<br> ";
             			}
     		}
     	if($text == '/start'){
-    			$messages =$mass['orders']['0']['fields'][0]['title'].': '.$mass['orders']['0']['fields'][0]['description']."\n ".$mass['orders']['0']['date']."\n ".$mass['orders']['0']['fields'][1]['title'].': '.$mass['orders']['0']['fields'][1]['description']."\n ".$mass['orders']['0']['fields'][2]['title'].': '.$mass['orders']['0']['fields'][2]['description']."\n ".$mass['orders']['0']['fields'][3]['title'].': '.$mass['orders']['0']['fields'][3]['description']."\n ".'ID '.$mass['orders']['0']['items'][0]['id']."\nНаименование: ".$mass['orders']['0']['items'][0]['title'].' '.$mass['orders']['0']['items'][0]['quantity']."шт. \n ".$mass['orders']['0']['items'][0]['price'].' '.$mass['orders']['0']['items'][0]['currency'];
+    		for($i=0;$i<=3;$i++){
+    			$messages =$mass['orders'][$i]['fields'][0]['title'].': '.$mass['orders'][$i]['fields'][0]['description']."\n ".$mass['orders'][$i]['date']."\n ".$mass['orders'][$i]['fields'][1]['title'].': '.$mass['orders'][$i]['fields'][1]['description']."\n ".$mass['orders'][$i]['fields'][2]['title'].': '.$mass['orders'][$i]['fields'][2]['description']."\n ".$mass['orders'][$i]['fields'][3]['title'].': '.$mass['orders'][$i]['fields'][3]['description']."\n ".'ID '.$mass['orders'][$i]['items'][0]['id']."\nНаименование: ".$mass['orders'][$i]['items'][0]['title'].' '.$mass['orders'][$i]['items'][0]['quantity']."шт. \n ".$mass['orders'][$i]['items'][0]['price'].' '.$mass['orders'][$i]['items'][0]['currency'];
     			sendMessage($website, $chatId, $messages);
-
-
+    		}
     	}
 
          function sendMessage($website, $chatId, $messages){
