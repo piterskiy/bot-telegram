54ba46e79c112b496ffc173b5d56bb8131033697
Pavlenko Svetoslav <Svet6os6laV6@yandex.ru>
2017-06-06 10:49:24 +0300

    1

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-$output = file_get_contents('php://input');
-file_put_contents('logs.txt',$output);
+
+    set_time_limit(0);
+
+    // Установка токена
+    $botToken = "257002841:AAF73D-lPHhpnqru8jp-R9qPq5FPM1KSOx8";
+    $website = "https://api.telegram.org/bot".$botToken;
+    // Получаем запрос от Telegram 
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, TRUE);
+    $message = $update["message"];
+    // Получаем внутренний номер чата Telegram и команду, введённую пользователем в   чате 
+    $chatId = $message["chat"]["id"];
+    $text = $message["text"];
+    // Пример обработки команды /start
+    if ($text == '/start') {
+        $welcomemessage = 'Welcome!!! Check IP/Email for spam giving "check IP/Email" command';
+       // Отправляем сформированное сообщение обратно в Telegram пользователю   
+       file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$welcomemessage);    
+    }    
+?>
