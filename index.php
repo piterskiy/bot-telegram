f12993947f6e38fbadfb501b0037e8cd010535b3
Pavlenko Svetoslav <Svet6os6laV6@yandex.ru>
2017-06-06 17:04:41 +0300

    fix

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $token = 'cxUUe9jdFLALvhZKa6niBC9UFvc5BSQRnDNxi9Ex';
         $mass = json_decode($content, TRUE);
             		for($i=0;$i<=6;$i++){
             			for ($k=0; $k <=6 ; $k++) { 
-    				echo $mass['orders'][$i]['fields'][0]['title'].': '.$mass['orders'][$i]['fields'][0]['description']."<br> ".$mass['orders'][$i]['date']."\n ".$mass['orders'][$i]['fields'][1]['title'].': '.$mass['orders'][$i]['fields'][1]['description']."\n ".$mass['orders'][$i]['fields'][2]['title'].': '.$mass['orders'][$i]['fields'][2]['description']."\n ".$mass['orders'][$i]['fields'][3]['title'].': '.$mass['orders'][$i]['fields'][3]['description']."\n ".'ID '.$mass['orders'][$i]['items'][0]['id']."\nНаименование: ".$mass['orders'][$i]['items'][0]['title'].' '.$mass['orders'][$i]['items'][0]['quantity']."шт. \n ".$mass['orders'][$i]['items'][0]['price'].' '.$mass['orders'][$i]['items'][0]['currency'];
+    				echo $mass['orders'][$i]['fields'][0]['title'].': '.$mass['orders'][$i]['fields'][0]['description']."<br> ".$mass['orders'][$i]['date']."<br> ".$mass['orders'][$i]['fields'][1]['title'].': '.$mass['orders'][$i]['fields'][1]['description']."<br> ".$mass['orders'][$i]['fields'][2]['title'].': '.$mass['orders'][$i]['fields'][2]['description']."<br> ".$mass['orders'][$i]['fields'][3]['title'].': '.$mass['orders'][$i]['fields'][3]['description']."<br> ".'ID '.$mass['orders'][$i]['items'][0]['id']."<br> Наименование: ".$mass['orders'][$i]['items'][0]['title'].' '.$mass['orders'][$i]['items'][0]['quantity']."шт. <br> ".$mass['orders'][$i]['items'][0]['price'].' '.$mass['orders'][$i]['items'][0]['currency']."<br> ";
             			}
     		}
     	if($text == '/start'){
