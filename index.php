1dcc4394b278ab9e388a89463af2f7895edf58ba
Pavlenko Svetoslav <Svet6os6laV6@yandex.ru>
2017-06-06 16:36:36 +0300

    fix

--- index.php
+++ index.php
@@ -11,11 +11,13 @@
 
 $sid ='hfgjixbl';
 $token = 'cxUUe9jdFLALvhZKa6niBC9UFvc5BSQRnDNxi9Ex';
-	$url = "http://apideliverycity.ru/api/orders?sid=".$sid."&token=".$token;
+	$url = "http://apideliverycity.ru/api/orders?sid=".$sid."&token=".$token.'&limit=6';
     	$content = file_get_contents($url);
         $mass = json_decode($content, TRUE);
+            		for($i=0;$i>=count($mass['orders']);$i++){
+    				echo $mass['orders'][$i];
+    		}
     	if($text == '/start'){
-
     			$messages =$mass['orders']['0']['fields'][0]['title'].': '.$mass['orders']['0']['fields'][0]['description']."\n ".$mass['orders']['0']['date']."\n ".$mass['orders']['0']['fields'][1]['title'].': '.$mass['orders']['0']['fields'][1]['description']."\n ".$mass['orders']['0']['fields'][2]['title'].': '.$mass['orders']['0']['fields'][2]['description']."\n ".$mass['orders']['0']['fields'][3]['title'].': '.$mass['orders']['0']['fields'][3]['description']."\n ".'ID '.$mass['orders']['0']['items'][0]['id']."\nНаименование: ".$mass['orders']['0']['items'][0]['title'].' '.$mass['orders']['0']['items'][0]['quantity']."шт. \n ".$mass['orders']['0']['items'][0]['price'].' '.$mass['orders']['0']['items'][0]['currency'];
     			sendMessage($website, $chatId, $messages);
 
